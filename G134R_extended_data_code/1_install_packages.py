799076c87ed32a03dccf46b0f4f4d9d6eab5d348
FilLieb <f.liebsch@uni-koeln.de>
2026-01-08 15:33:18 +0100

    starting_using_models

--- G134R_extended_data_code/1_install_packages.py
+++ G134R_extended_data_code/1_install_packages.py
@@ -1,15 +1,24 @@
 # code was run under Python 3.10.8 in virtual environment
 
 # packages were installed using pip
-#apoc 0.12.0
-#pip install apoc 
 
+# apoc 0.12.0
+# pip install apoc 
 
+# scikit-image 0.26.0
+# pip install scikit-image 
 
+# pyclesperanto-prototype 0.24.5
+# pip install pyclesperanto-prototype 
 
+# numpy 2.4.0
+# pip install numpy
 
-#scikit-image 0.26.0
-#pip install scikit-image 
+# pandas 2.3.3
+# pip install pandas
 
-#pyclesperanto-prototype 0.24.5
-#pip install pyclesperanto-prototype 
+# scipy 1.16.2
+# pip install scipy
+
+# napari_simpleitk_image_processing 0.4.9
+# pip install napari_simpleitk_image_processing
